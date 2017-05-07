0aec61f8cdc915c94df2bb6f7398c6827711a54b
Ryan Lacroix <ryan.lacroix53@gmail.com>
2017-05-06 23:26:36 -0400

    cleaned up code

--- public/client.js
+++ public/client.js
@@ -1,8 +1,8 @@
-window.onload = function() {
+window.onload = function () {
     loadFlasher();
     $('#submit-search').click(newSearch);
     $.ajax({
-        method : "GET",
+        method: "GET",
         url: '/search/Rush',
         success: drawGraph
     });
@@ -12,11 +12,11 @@ function drawGraph(data) {
     $('#loading-screen').remove();
     var container = document.getElementById('graph');
     var options = {
-        layout: {improvedLayout:false},
-        physics: {stabilization: false},
-        nodes: {shape: 'triangle'}
-      };
-      console.log(data);
+        layout: { improvedLayout: false },
+        physics: { stabilization: false },
+        nodes: { shape: 'triangle' }
+    };
+    console.log(data);
     var graph = new vis.Network(container, JSON.parse(data), options);
 }
 
@@ -29,22 +29,21 @@ function newSearch() {
     $('body').append('<div id="graph"></div>');
     if (query.length > 0) {
         $.ajax({
-            method : "GET",
+            method: "GET",
             url: '/search/' + query,
             success: drawGraph
         });
     }
-    
 }
 
 function loadFlasher() {
-    setInterval(function() {
-        $('#loading-screen').animate({ 
-            opacity: 0.1, 
-        }, 500, 'linear') 
-        .animate({ 
-            opacity: 1 
-        }, 500, 'linear');
-    },1000);
+    setInterval(function () {
+        $('#loading-screen').animate({
+            opacity: 0.1,
+        }, 500, 'linear')
+            .animate({
+                opacity: 1
+            }, 500, 'linear');
+    }, 1000);
 }
 
